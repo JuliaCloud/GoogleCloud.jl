304caf178162690fdcf66bdec1ab7f2ba16f4de4
Josh Bode <joshbode@fastmail.com>
2016-09-29 10:46:11 +1000

    more doc config

--- docs/make.jl
+++ docs/make.jl
@@ -1,13 +1,16 @@
 #! /usr/bin/env julia
 
-import Documenter
+using Documenter
 using GoogleCloud
 
-Documenter.makedocs(
+makedocs(
     modules=[GoogleCloud], doctest=true,
     sitename="Google Cloud JSON APIs",
     format=Documenter.Formats.HTML,
-    deps=Deps.pip("mkdocs", "python-markdown-math"),
+)
+
+deploydocs(
+    deps=Deps.pip("pygments", "mkdocs", "python-markdown-math"),
     repo="github.com/joshbode/GoogleCloud.jl.git",
     julia="0.5"
 )
