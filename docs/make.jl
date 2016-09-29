f405bb90f07a10a1b6c65fa2bab726cde772c721
Josh Bode <joshbode@fastmail.com>
2016-09-29 10:24:44 +1000

    more doc build setup

--- docs/make.jl
+++ docs/make.jl
@@ -6,5 +6,8 @@ using GoogleCloud
 Documenter.makedocs(
     modules=[GoogleCloud], doctest=true,
     sitename="Google Cloud JSON APIs",
-    format=Documenter.Formats.HTML
+    format=Documenter.Formats.HTML,
+    deps=Deps.pip("mkdocs", "python-markdown-math"),
+    repo="github.com/joshbode/GoogleCloud.jl.git",
+    julia="0.5"
 )
