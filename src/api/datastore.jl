d37801c992eed12877f23ca41189eb32bd3a4d0f
Josh Bode <joshbode@fastmail.com>
2017-04-11 17:25:59 +1000

    functions to assist with wrapping values for datastore

--- src/api/datastore.jl
+++ src/api/datastore.jl
@@ -3,26 +3,82 @@ Google Cloud Datastore API
 """
 module _datastore
 
-export datastore, DatastoreValueType
+export datastore
 
 using ..api
 using ...root
 
-@enum(DatastoreValueType,
-    nullValue,
-    booleanValue,
-    integerValue,
-    doubleValue,
-    timestampValue,
-    keyValue,
-    stringValue,
-    blobValue,
-    geoPointValue,
-    entityValue,
-    arrayValue
-)
-value_type_map = Dict(string(x) => x for x in instances(DatastoreValueType))
-Base.convert(::Type{DatastoreValueType}, x::AbstractString) = haskey(value_type_map, x) ? value_type_map[x] : Base.error("Unknown Datastore value type")
+"""Enumerations"""
+module types
+    export ValueType, OperatorType, wrap, unwrap
+
+    using Base.Dates
+
+    import JSON
+
+    """Datastore value types"""
+    @enum(ValueType,
+        nullValue,
+        booleanValue, integerValue, doubleValue, timestampValue,
+        keyValue, stringValue, blobValue,
+        geoPointValue, entityValue, arrayValue
+    )
+    value_type_map = Dict(Symbol(x) => x for x in instances(ValueType))
+    Base.convert(::Type{ValueType}, x::Symbol) = haskey(value_type_map, x) ? value_type_map[x] : error("Unknown Datastore value type: $x")
+    JSON.lower(x::ValueType) = string(x)
+    for s in instances(ValueType)
+        @eval export $(Symbol(s))
+    end
+
+    """Datastore comparison operator types"""
+    @enum(OperatorType, LESS_THAN, LESS_THAN_OR_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, EQUAL, HAS_ANCESTOR)
+    operator_type_map = Dict(Symbol(x) => x for x in instances(OperatorType))
+    Base.convert(::Type{OperatorType}, x::Symbol) = haskey(operator_type_map, x) ? operator_type_map[x] : error("Unknown Datastore comparison operator type: $x")
+    JSON.lower(x::OperatorType) = string(x)
+    for s in instances(OperatorType)
+        @eval export $(Symbol(s))
+    end
+
+    value_types = Dict{DataType, ValueType}(
+        Bool => booleanValue,
+        Integer => integerValue,
+        AbstractFloat => doubleValue,
+        TimeType => timestampValue,
+        AbstractString => stringValue,
+        Char => stringValue,
+        Enum => stringValue,
+        Void => nullValue,
+    )
+    function wrap(x)
+        T = typeof(x)
+        if T <: AbstractArray
+            return Dict(arrayValue => Dict(:values => map(wrap, x)))
+        end
+        # move up type hierarchy until we get a hit
+        while T !== Any
+            if haskey(value_types, T)
+                return Dict(value_types[T] => x)
+            end
+            T = supertype(T)
+        end
+        Dict(blobValue => base64encode(JSON.json(x)))
+    end
+    function unwrap(x)
+        value_type, value = first(x)
+        value_type = convert(ValueType, value_type)
+        if value_type == timestampValue
+            DateTime(chop(value), ISODateTimeFormat)
+        elseif value_type == integerValue
+            parse(Int64, value)
+        elseif value_type == arrayValue
+            map(unwrap, value[:values])
+        elseif value_type == blobValue
+            JSON.parse(String(base64decode(value)))
+        else
+            value
+        end
+    end
+end
 
 """
 Google Cloud Datastore API root.
