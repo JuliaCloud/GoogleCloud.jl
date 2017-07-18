584a7b78dc34e9cc291c9f245fa04d805b9fb112
Josh Bode <josh.bode@healthcareresourceoptimization.com>
2017-07-18 14:17:12 +1000

    fix typo

--- src/api/api.jl
+++ src/api/api.jl
@@ -268,7 +268,7 @@ function execute(session::GoogleSession, resource::APIResource, method::APIMetho
             )
         catch e
             if isa(e, Base.UVError) && e.code in (Base.UV_ECONNRESET, Base.UV_ECONNREFUSED, Base.UV_ECONNABORTED, Base.UV_EPIPE, Base.UV_ETIMEDOUT)
-            elseif isa(e, MbedTLS.MbedExeption) && e.ret in (MbedTLS.MBEDTLS_ERR_SSL_TIMEOUT, MbedTLS.MBEDTLS_ERR_SSL_CONN_EOF)
+            elseif isa(e, MbedTLS.MbedException) && e.ret in (MbedTLS.MBEDTLS_ERR_SSL_TIMEOUT, MbedTLS.MBEDTLS_ERR_SSL_CONN_EOF)
             else
                 rethrow(e)
             end
