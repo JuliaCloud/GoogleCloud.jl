82a5f8cab071c110672cb6b4d8a4e9b9b7b31a19
Jacob Quinn <quinn.jacobd@gmail.com>
2022-07-04 11:05:19 -0600

    fix docs deploy

--- docs/make.jl
+++ docs/make.jl
@@ -4,12 +4,12 @@ using Documenter
 using GoogleCloud
 
 makedocs(
-    modules=[GoogleCloud], doctest=true,
+    doctest=true,
     sitename="Google Cloud JSON APIs"
 )
 
 deploydocs(
     deps=Deps.pip("pygments", "mkdocs", "python-markdown-math"),
-    repo="github.com/joshbode/GoogleCloud.jl.git",
-    julia="0.5"
+    repo="github.com/JuliaCloud/GoogleCloud.jl.git",
+    push_preview = true,
 )
