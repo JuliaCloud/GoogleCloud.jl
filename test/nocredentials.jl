ea007068fc6e0378cf10ce5495e304da314fea43
Nicolau Leal Werneck <nwerneck@gmail.com>
2023-06-06 23:33:49 +0200

    fixing test with new working json decoding

--- test/nocredentials.jl
+++ test/nocredentials.jl
@@ -7,13 +7,16 @@ creds = NoCredentials()
 
 session = GoogleSession(creds, ["devstorage.full_control"])
 bucketName = "atari-replay-datasets"
-@show mydata = GoogleCloud.storage(:Object, :list, bucketName; prefix="dqn/Pong/1/replay_logs/\$store\$_action", session=session)
-fileList = JSON.parse(IOBuffer(mydata))
+prefix="dqn/Pong/1/replay_logs/\$store\$_action"
 
-parities = map(fileList["items"]) do item
-    reduce(⊻, GoogleCloud.storage(:Object, :get, bucketName, item["name"], session=session))
+fileList = GoogleCloud.storage(:Object, :list, bucketName; prefix, session)
+
+parities = map(fileList) do item
+    reduce(⊻, GoogleCloud.storage(:Object, :get, bucketName, item[:name]; session))
 end
 
+EXPECTED_PARITY = 0xf1
+
 @testset "NoCredentials" begin
-    @test reduce(⊻, fetch.(parities)) == 0xf1
+    @test reduce(⊻, fetch.(parities)) == EXPECTED_PARITY
 end
