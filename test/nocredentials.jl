5fd90dd5e992e61772a0d83d594fcf072f5c6f42
Nicolau Leal Werneck <nwerneck@gmail.com>
2023-06-06 22:52:53 +0200

    rebasing on master and adding a testset statement

--- test/nocredentials.jl
+++ test/nocredentials.jl
@@ -3,14 +3,16 @@ using Test
 using GoogleCloud
 using JSON
 
-creds = NoCredentials()
+@testset "NoCredentials" begin
+    creds = NoCredentials()
 
-session = GoogleSession(creds, ["devstorage.full_control"])
-bucketName = "atari-replay-datasets"
-fileList = GoogleCloud.storage(:Object, :list, bucketName; prefix="dqn/Pong/1/replay_logs/\$store\$_action", session=session) |> IOBuffer |> JSON.parse
+    session = GoogleSession(creds, ["devstorage.full_control"])
+    bucketName = "atari-replay-datasets"
+    fileList = GoogleCloud.storage(:Object, :list, bucketName; prefix="dqn/Pong/1/replay_logs/\$store\$_action", session=session) |> IOBuffer |> JSON.parse
 
-parities = map(fileList["items"]) do item
-    reduce(⊻, GoogleCloud.storage(:Object, :get, bucketName, item["name"], session=session))
+    parities = map(fileList["items"]) do item
+        reduce(⊻, GoogleCloud.storage(:Object, :get, bucketName, item["name"], session=session))
+    end
+
+    @test reduce(⊻, fetch.(parities)) == 0xf1
 end
-
-@test reduce(⊻, fetch.(parities)) == 0xf1
